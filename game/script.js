51fae36022ebb0e090061f5315d09b7f189872ff
ngaiohawkins <114262503+ngaiohawkins@users.noreply.github.com>
2024-10-30 15:46:19 -0400

    Update script.js

--- game/script.js
+++ game/script.js
@@ -56,8 +56,8 @@ function jump() {
 
 document.addEventListener('keydown', function(event) {
   document.getElementById('rules').style.display = 'none';
-  document.getElementById('spike1').style.display = 'none';
-  document.getElementById('spike2').style.display = 'none';
+  document.getElementById('spike1').style.display = 'block';
+  document.getElementById('spike2').style.display = 'block';
     if (event.code === 'Space' && onFloor) {
       jump();
     }
